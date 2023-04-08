e8adfe9f573446c02417b17a0a729b09f50932fc
EthanL06 <ethanlanting@gmail.com>
2023-04-08 12:19:47 -0500

    changes

--- pages/support.tsx
+++ pages/support.tsx
@@ -1,22 +1,42 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Link from "next/link";
-
+import { motion } from "framer-motion";
 type Props = {};
 const Support = (props: Props) => {
   return (
     <div className="mx-auto flex h-full min-h-screen max-w-6xl flex-col items-center p-4">
       <Navbar />
       <div className="relative mb-16">
-        <h1 className="mt-6 text-center text-5xl font-bold ">Support Us</h1>
+        <motion.h1
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          viewport={{ once: true }}
+          className="mt-6 text-center text-5xl font-bold "
+        >
+          Support Us
+        </motion.h1>
 
-        <div className="absolute -top-2 -right-20 hidden lg:block">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1 }}
+          viewport={{ once: true }}
+          className="absolute -top-2 -right-20 hidden lg:block"
+        >
           <img src="/underline.png" />
-        </div>
+        </motion.div>
       </div>
 
-      <div className="mt-7 mb-32 flex max-w-sm grid-cols-3 grid-rows-1 flex-1 flex-wrap justify-center gap-32 lg:gap-4 lg:grid lg:max-w-none">
-        <div className="relative flex flex-col items-center justify-between rounded-[1rem] border-4 border-white bg-[#FFE8D3] py-6 px-4 text-center gap-y-4">
+      <div className="mt-7 mb-32 flex max-w-md grid-cols-3 grid-rows-1 flex-1 flex-wrap justify-center gap-20 lg:gap-4 lg:grid lg:max-w-none">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          viewport={{ once: true }}
+          className="relative flex flex-col items-center justify-between rounded-[1rem] border-4 border-white bg-[#FFE8D3] py-6 px-4 text-center gap-y-4"
+        >
           <img
             src="/1.png"
             className="absolute -top-16 left-1/2 -translate-x-1/2"
@@ -29,13 +49,19 @@ const Support = (props: Props) => {
           </p>
           <Link
             href={"/"}
-            className="mt-4 rounded-full bg-white  px-4 py-3 text-lg font-semibold"
+            className="mt-4 rounded-full bg-white  px-4 py-3 text-lg font-semibold transition-transform hover:scale-95 active:scale-90"
           >
             Our Posts
           </Link>
-        </div>
+        </motion.div>
 
-        <div className="relative flex w-full flex-col items-center justify-between rounded-[1rem] border-4 border-white bg-[#CFF0F9] py-6 px-4 text-center gap-y-4">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          viewport={{ once: true }}
+          className="relative flex w-full flex-col items-center justify-between rounded-[1rem] border-4 border-white bg-[#CFF0F9] py-6 px-4 text-center gap-y-4"
+        >
           <img
             src="/2.png"
             className="absolute -top-16 left-1/2 -translate-x-1/2"
@@ -48,13 +74,19 @@ const Support = (props: Props) => {
           </p>
           <Link
             href={"/"}
-            className="mt-4 rounded-full bg-white  px-4 py-3 text-lg font-semibold"
+            className="mt-4 rounded-full bg-white  px-4 py-3 text-lg font-semibold transition-transform hover:scale-95 active:scale-90"
           >
             More Info
           </Link>
-        </div>
+        </motion.div>
 
-        <div className="relative flex flex-col items-center justify-between rounded-[1rem] border-4 border-white bg-[#F6D1CC] py-6 px-4 text-center gap-y-4">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.7 }}
+          viewport={{ once: true }}
+          className="relative flex flex-col items-center justify-between rounded-[1rem] border-4 border-white bg-[#F6D1CC] py-6 px-4 text-center gap-y-4"
+        >
           <img
             src="/3.png"
             className="absolute -top-16 left-1/2 -translate-x-1/2"
@@ -69,11 +101,11 @@ const Support = (props: Props) => {
           </p>
           <Link
             href={"/"}
-            className="mt-4 rounded-full bg-white  px-4 py-3 text-lg font-semibold"
+            className="mt-4 rounded-full bg-white  px-4 py-3 text-lg font-semibold transition-transform hover:scale-95 active:scale-90"
           >
             Amazon Wishlist
           </Link>
-        </div>
+        </motion.div>
       </div>
       <Footer />
     </div>
